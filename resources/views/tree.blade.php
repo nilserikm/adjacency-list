84ff59120c907990990e25b84c6e9832eb011fc8
Nils Erik Martinussen <nils@kvalitetskontroll.no>
2018-11-02 15:53:30 +0100

    Create tree-blade for simple visual representation

---
+++ resources/views/tree.blade.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <h1>adjacency list</h1>
+        <div>
+            <h2>tree</h2>
+            count: {{ $count }} <br/>
+            duration: {{ $duration }} <br/>
+            add single: {{ $addDuration }} <br/>
+            add count: {{ $addCount }} <br/>
+            delete single: {{ $deleteDuration }} <br/>
+            delete count: {{ $deleteCount }} <br/>
+            delete subtree: {{ $deleteSubtreeDuration }} <br/>
+            subtree count: {{ $subtreeCount }} <br/>
+            root: {{ $root }} <br/>
+            after count: {{ $afterCount }} <br/>
+        </div>
+    </body>
+</html>
