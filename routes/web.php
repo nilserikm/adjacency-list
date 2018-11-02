6be18385ba149a0ac714bc3a9324ebb74ac86173
Nils Erik Martinussen <nils@kvalitetskontroll.no>
2018-11-02 15:52:22 +0100

    Add route for welcome view

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'WelcomeController@welcome');
+//Route::get('/', function () {
+//    return view('welcome');
+//});
