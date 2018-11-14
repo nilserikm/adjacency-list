8cc1a32e5a485783107ba8cf304f1e3b73e128c0
Nils Erik Martinussen <nils@kvalitetskontroll.no>
2018-11-14 10:10:34 +0100

    Set seeder to seed approx 3.5k nodes with buildings as the start point

--- database/seeds/NodeTableSeeder.php
+++ database/seeds/NodeTableSeeder.php
@@ -13,44 +13,62 @@ class NodeTableSeeder extends Seeder
     {
         $start = microtime(true);
 
-        $numBuildings = 5;
-        $numPhases = 5;
+        // seed time ~20s
+        // node count ~3.5k
+        // ONE BUILDING (building as root)
+        $numPhases = 7;
         $numApartments = 20;
-        $numActivities = 20;
-        $numLeaves = 0;
+        $numActivities = 25;
+        $numUnnamedFirst = 0;
+        $numUnnamedSecond = 0;
 
-        // LARGE VERSION
-        // level 0 node (root)
-        // "project"
+        // "building"
         $root = new \App\node();
-        $root->title = "root node";
+        $root->title = "RH-1";
         $root->save();
 
-        // level 1 children
-        // "buildings"
+        // "phases"
         $rootChildren = [];
-        for ($i = 0; $i < $numBuildings; $i++) {
-            $node = new \App\node(['title' => 'level1 node ' . $i]);
+        $phases = ["Rigg og drift", "Tett hus", "Utvendig", "Innvendig", "Innredning", "Ikke-navngitt fase", "Elementproduksjon"];
+        for ($i = 0; $i < $numPhases; $i++) {
+            $node = new \App\node(['title' => $phases[$i]]);
             array_push($rootChildren, $node);
         }
         $root->addChildren($rootChildren);
 
         foreach ($root->getChildren() as $child) {
-            // level 2 children
-            // "phases"
+            // "apartments"
             $level1Children = [];
-            for ($i = 0; $i < $numPhases; $i++) {
-                $node = new \App\node(['title' => 'level2 node ' . $i]);
+            for ($i = 0; $i < $numApartments; $i++) {
+                if ($i < 10) {
+                    $num = "0" . $i;
+                } else {
+                    $num = $i;
+                }
+
+                $node = new \App\node(['title' => 'H01' . $num]);
                 array_push($level1Children, $node);
             }
             $child->addChildren($level1Children);
 
             foreach($level1Children as $level1Child) {
-                // level 3 children
-                // "apartments"
+                // "activities"
                 $level2Children = [];
-                for ($i = 0; $i < $numApartments; $i++) {
-                    $node = new \App\node(['title' => 'level3 node ' . $i]);
+                $activities = [
+                    "Maling", "Snekring", "Listing", "Dobling", "Tripling",
+                    "Sparkling", "Trekking", "Elektriker", "Tapetsering", "Rydding",
+                    "Kobling", "Rørlegger", "Tørking", "Vasking", "Gulvlegging",
+                    "Ventilasjon", "Gipsing", "Banking", "Tømring", "Møblering",
+                    "Membran", "Dusj og toalett", "Kjøkken", "Sikring balkong", "Lamper"
+                ];
+                for ($i = 0; $i < $numActivities; $i++) {
+                    if ($i < count($activities)) {
+                        $activity = $activities[$i];
+                    } else {
+                        $activities = "Ikke-navngitt aktivitet";
+                    }
+
+                    $node = new \App\node(['title' => $activity]);
                     array_push($level2Children, $node);
                 }
                 $level1Child->addChildren($level2Children);
@@ -59,7 +77,7 @@ class NodeTableSeeder extends Seeder
                     // level 4 children
                     // "activities"
                     $level3Children = [];
-                    for ($i = 0; $i < $numActivities; $i++) {
+                    for ($i = 0; $i < $numUnnamedFirst; $i++) {
                         $node = new \App\node(['title' => 'level4 node ' . $i]);
                         array_push($level3Children, $node);
                     }
@@ -69,7 +87,7 @@ class NodeTableSeeder extends Seeder
                         // level 5 children
                         // "leaves"
                         $level4Children = [];
-                        for ($i = 0; $i < $numLeaves; $i++) {
+                        for ($i = 0; $i < $numUnnamedSecond; $i++) {
                             $node = new \App\node(['title' => 'level5 node ' . $i]);
                             array_push($level4Children, $node);
                         }
