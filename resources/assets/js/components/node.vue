3b802eda621a037719d48a6c27eb0583a31d41dc
Nils Erik Martinussen <nils@kvalitetskontroll.no>
2018-11-14 15:12:46 +0100

    Add recursive node component

---
+++ resources/assets/js/components/node.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="nodi-entry">
+        <span class="node-title">{{ title }}</span>
+        <button @click="expand">Expand</button>
+        <node
+            v-for="node in nodes"
+            :key="node.id"
+            v-if="display"
+            :nodes="node.children"
+            :title="node.title"
+            :estimate="node.estimate"
+            :show="false"
+        >
+        </node>
+    </div>
+</template>
+
+<style lang="scss">
+    .hideNode {
+        display: block;
+    }
+
+    .node-title {
+        margin-right: 200px;
+    }
+
+    .nodi-entry {
+        margin-left: 20px;
+    }
+</style>
+
+<script>
+    export default {
+        props: [
+            'nodes',
+            'title',
+            'estimate',
+            'show'
+        ],
+        name: 'node',
+        data() {
+            return {
+                display: null
+            }
+        },
+
+        mounted() {
+            this.display = this.show;
+        },
+
+        methods: {
+            expand() {
+                this.display = !this.display;
+            }
+        }
+    }
+</script>
