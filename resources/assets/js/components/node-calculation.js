8b88369111d7774b4840c1f2c66a1ec3cb9d8af8
Nils Erik Martinussen <nils@kvalitetskontroll.no>
2018-11-14 10:25:22 +0100

    Updated some stuff in the node-calculation view

--- resources/assets/js/components/node-calculation.js
+++ resources/assets/js/components/node-calculation.js
@@ -5,14 +5,11 @@ export default {
         return {
             treeLevel1: 1,
             treeLevel2: 5,
-            treeLevel3: 5,
-            treeLevel4: 20,
+            treeLevel3: 7,
+            treeLevel4: 25,
             treeLevel5: 20,
             treeLevel6: 0,
-            numberOfNodes: null,
-            rootChildNodes: null,
-            level3Nodes: null,
-            level4Nodes: null
+            numberOfNodes: null
         }
     },
 
@@ -23,33 +20,6 @@ export default {
     methods: {
         calculateNodes() {
             this.numberOfNodes = this.calculateTotal();
-            this.rootChildNodes = this.calculateRootChild();
-            this.level3Nodes = this.calculateLevel3Nodes();
-            this.level4Nodes = this.calculateLevel4Nodes();
-        },
-
-        calculateLevel4Nodes() {
-            let l4 = 1;
-            let l5 = l4 * this.treeLevel5;
-            let l6 = l5 * this.treeLevel6;
-            return parseInt(l4) + parseInt(l5) + parseInt(l6);
-        },
-
-        calculateLevel3Nodes() {
-            let l3 = 1;
-            let l4 = l3 * this.treeLevel4;
-            let l5 = l4 * this.treeLevel5;
-            let l6 = l5 * this.treeLevel6;
-            return parseInt(l3) + parseInt(l4) + parseInt(l5) + parseInt(l6);
-        },
-
-        calculateRootChild() {
-            let l2 = 1;
-            let l3 = l2 * this.treeLevel3;
-            let l4 = l3 * this.treeLevel4;
-            let l5 = l4 * this.treeLevel5;
-            let l6 = l5 * this.treeLevel6;
-            return parseInt(l2) + parseInt(l3) + parseInt(l4) + parseInt(l5) + parseInt(l6);
         },
 
         calculateTotal() {
--- resources/assets/js/components/node-calculation.vue
+++ resources/assets/js/components/node-calculation.vue
@@ -68,39 +68,6 @@
             />
         </div>
 
-        <div class="node-result number-level-4">
-            <label>Number of Nodes: Level 4</label>
-            <input
-                ref="level4Nodes"
-                class="form-control"
-                type="number"
-                v-model="level4Nodes"
-                disabled
-            >
-        </div>
-
-        <div class="node-result number-level-3">
-            <label>Number of Nodes: Level 3</label>
-            <input
-                ref="level3Nodes"
-                class="form-control"
-                type="number"
-                v-model="level3Nodes"
-                disabled
-            >
-        </div>
-
-        <div class="node-result number-root-child">
-            <label>Number of Nodes: Root child</label>
-            <input
-                ref="rootChildNodes"
-                class="form-control"
-                type="number"
-                v-model="rootChildNodes"
-                disabled
-            >
-        </div>
-
         <div class="node-result">
             <label>Number of Nodes: Tree</label>
             <input
