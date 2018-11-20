3e61bac71eb6a41e27c8b99a2d0c03ebf76f9479
Nils Erik Martinussen <nils@kvalitetskontroll.no>
2018-11-20 12:16:30 +0100

    Add comments to duplicate

--- app/Tree.php
+++ app/Tree.php
@@ -2,10 +2,18 @@
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Collection;
+use \App\node;
 
 class Tree extends Model
 {
-    public static function duplicate(\App\node $base, Collection $children)
+    /**
+     * If children is empty then the base-node is returned, otherwise the base-
+     * node is returned with a clone of the children attached
+     * @param node $base
+     * @param Collection $children
+     * @return node
+     */
+    public static function duplicate(node $base, Collection $children)
     {
         foreach($children as $child) {
             $clone = $child->replicate();
