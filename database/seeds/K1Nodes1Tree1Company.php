7de172466e7cf1a7e7e008568f5bc30fd310accd
Nils Erik Martinussen <nils@kvalitetskontroll.no>
2018-11-20 12:00:03 +0100

    Add new seeder for AL

---
+++ database/seeds/K1Nodes1Tree1Company.php
@@ -0,0 +1,154 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class K1Nodes1Tree1Company extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $start = microtime(true);
+
+//        $numBuildings = 1;
+//        $numPhases = 5;
+//        $numApartments = 7;
+//        $numActivities = 20;
+//        $numLastLevel = 25;
+
+        $numBuildings = 1;
+        $numPhases = 2;
+        $numApartments = 5;
+        $numActivities = 10;
+        $numLastLevel = 0;
+
+        $companyId = 1;
+        $cloneNodeId = null;
+        $cloneParentId = null;
+
+        for ($iteration = 0; $iteration < 10; $iteration++) {
+            $this->command->info('starting iteration: ' . $iteration);
+            $iStart = microtime(true);
+
+            $companyId = $companyId + $iteration;
+
+            for ($i = 0; $i < $numBuildings; $i++) {
+                $building = new \App\node();
+                $building->title = 'building ' . $i;
+                $building->company_id = $companyId;
+                $building->save();
+                $cloneParentId = $building->id;
+
+                $phases = [];
+                for ($j = 0; $j < $numPhases; $j++) {
+                    $node = new \App\node([
+                        'title' => 'phase ' . $j,
+                        'company_id' => $companyId
+                    ]);
+                    array_push($phases, $node);
+                }
+                $building->addChildren($phases);
+
+                foreach ($building->getChildren() as $phase) {
+                    $cloneNodeId = $phase->id;
+                    $apartments = [];
+                    for ($k = 0; $k < $numApartments; $k++) {
+                        $node = new \App\Node([
+                            'title' => 'apartment ' . $k,
+                            'company_id' => $companyId
+                        ]);
+                        array_push($apartments, $node);
+                    }
+                    $phase->addChildren($apartments);
+
+                    foreach ($phase->getChildren() as $apartment) {
+                        $activities = [];
+                        for ($n = 0; $n < $numActivities; $n++) {
+                            $node = new \App\Node([
+                                'title' => 'activity ' . $n,
+                                'company_id' => $companyId
+                            ]);
+                            array_push($activities, $node);
+                        }
+                        $apartment->addChildren($activities);
+
+                        foreach ($apartment->getChildren() as $activity) {
+                            $lastLevels = [];
+                            for ($m = 0; $m < $numLastLevel; $m++) {
+                                $node = new \App\Node([
+                                    'title' => 'last-level ' . $m,
+                                    'company_id' => $companyId
+                                ]);
+                                array_push($lastLevels, $node);
+                            }
+                            $activity->addChildren($lastLevels);
+                        }
+                    }
+                }
+            }
+
+            $iTime = microtime(true) - $iStart;
+            $this->command->info("seed time: " . $iTime);
+
+            $cloneNode = \App\node::find($cloneNodeId);
+            $copy = $cloneNode->replicate();
+            $copy->save();
+
+            $cloneStart = microtime(true);
+            $duplicate = function($base, $children) use (&$duplicate) {
+                foreach($children as $child) {
+                    $clone = $child->replicate();
+                    $base->addChild($clone);
+
+                    if ($child->hasChildren()) {
+                        $duplicate($clone, $child->getChildren());
+                    }
+                }
+
+                return $base;
+            };
+
+            $base = $duplicate($copy, $cloneNode->getChildren());
+            $parent = \App\node::where('id', $cloneParentId)
+                ->where('company_id', $companyId)
+                ->first();
+            $parent->addChild($base);
+            $cloneTime = microtime(true) - $cloneStart;
+            $this->command->info('clone time: ' . $cloneTime);
+
+            $rStart = microtime(true);
+            $tempRoot = \App\node::find($cloneParentId);
+            $tempTree = $tempRoot->getDescendantsTree();
+            $readTime = microtime(true) - $rStart;
+            $this->command->info('read tree size: ' . ($tempRoot->countDescendants() + 1));
+            $this->command->info('read time: ' . $readTime);
+
+            $cloneNodeParent = \App\node::find($cloneParentId);
+
+            $clonedNum = ($cloneNode->countDescendants() + 1);
+            $treeNum = ($cloneNodeParent->countDescendants() + 1);
+            $tableNum = \App\Node::count();
+
+            $this->command->info('number of added nodes: ' . $clonedNum);
+            $this->command->info('nodes in tree: ' . $treeNum);
+            $this->command->info('nodes in table: ' . $tableNum);
+            \Illuminate\Support\Facades\DB::table('stats')->insert([
+                'clone_time' => $cloneTime,
+                'cloned_num' => $clonedNum,
+                'tree_num' => $treeNum,
+                'table_num' => $tableNum,
+                'read_time' => $readTime
+            ]);
+            $this->command->info("-----------------------------------------");
+            $this->command->info('');
+            $this->command->info('');
+        }
+
+        $end = microtime(true);
+
+        $this->command->info("migration time: " . ($end - $start));
+    }
+}
