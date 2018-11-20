b9da8ddf2962c977fdfd75212071c64836396f03
Nils Erik Martinussen <nils@kvalitetskontroll.no>
2018-11-20 12:07:50 +0100

    Make seeder more flexible and easier to work with

--- database/seeds/K1Nodes1Tree1Company.php
+++ database/seeds/K1Nodes1Tree1Company.php
@@ -4,6 +4,12 @@ use Illuminate\Database\Seeder;
 
 class K1Nodes1Tree1Company extends Seeder
 {
+
+    public $runStats = true;
+    public $runDuplication = false;
+    public $numIterations = 10;
+    public $companyId = 1;
+
     /**
      * Run the database seeds.
      *
@@ -13,12 +19,15 @@ class K1Nodes1Tree1Company extends Seeder
     {
         $start = microtime(true);
 
+        // the number of nodes in each level for data-resemblance to a "real"
+        // Markhus-project
 //        $numBuildings = 1;
 //        $numPhases = 5;
 //        $numApartments = 7;
 //        $numActivities = 20;
 //        $numLastLevel = 25;
 
+        // the number of nodes in each level for a "basic" setup
         $numBuildings = 1;
         $numPhases = 2;
         $numApartments = 5;
@@ -29,11 +38,9 @@ class K1Nodes1Tree1Company extends Seeder
         $cloneNodeId = null;
         $cloneParentId = null;
 
-        for ($iteration = 0; $iteration < 10; $iteration++) {
+        for ($iteration = 0; $iteration < $this->numIterations; $iteration++) {
             $this->command->info('starting iteration: ' . $iteration);
             $iStart = microtime(true);
-
-            $companyId = $companyId + $iteration;
 
             for ($i = 0; $i < $numBuildings; $i++) {
                 $building = new \App\node();
@@ -90,61 +97,67 @@ class K1Nodes1Tree1Company extends Seeder
                 }
             }
 
-            $iTime = microtime(true) - $iStart;
-            $this->command->info("seed time: " . $iTime);
+            if ($this->runStats) {
+                $iTime = microtime(true) - $iStart;
+                $this->command->info("seed time: " . $iTime);
 
-            $cloneNode = \App\node::find($cloneNodeId);
-            $copy = $cloneNode->replicate();
-            $copy->save();
+                $cloneNode = \App\node::find($cloneNodeId);
+                $copy = $cloneNode->replicate();
+                $copy->save();
 
-            $cloneStart = microtime(true);
-            $duplicate = function($base, $children) use (&$duplicate) {
-                foreach($children as $child) {
-                    $clone = $child->replicate();
-                    $base->addChild($clone);
+                if ($this->runDuplication) {
+                    $cloneStart = microtime(true);
+                    $duplicate = function($base, $children) use (&$duplicate) {
+                        foreach($children as $child) {
+                            $clone = $child->replicate();
+                            $base->addChild($clone);
 
-                    if ($child->hasChildren()) {
-                        $duplicate($clone, $child->getChildren());
-                    }
+                            if ($child->hasChildren()) {
+                                $duplicate($clone, $child->getChildren());
+                            }
+                        }
+
+                        return $base;
+                    };
+
+                    $base = $duplicate($copy, $cloneNode->getChildren());
+                    $parent = \App\node::where('id', $cloneParentId)
+                        ->where('company_id', $companyId)
+                        ->first();
+                    $parent->addChild($base);
+                    $cloneTime = microtime(true) - $cloneStart;
+                    $this->command->info('clone time: ' . $cloneTime);
                 }
 
-                return $base;
-            };
+                $rStart = microtime(true);
+                $tempRoot = \App\node::find($cloneParentId);
+                $tempTree = $tempRoot->getDescendantsTree();
+                $readTime = microtime(true) - $rStart;
+                $this->command->info('read tree size: ' . ($tempRoot->countDescendants() + 1));
+                $this->command->info('read time: ' . $readTime);
 
-            $base = $duplicate($copy, $cloneNode->getChildren());
-            $parent = \App\node::where('id', $cloneParentId)
-                ->where('company_id', $companyId)
-                ->first();
-            $parent->addChild($base);
-            $cloneTime = microtime(true) - $cloneStart;
-            $this->command->info('clone time: ' . $cloneTime);
+                $cloneNodeParent = \App\node::find($cloneParentId);
 
-            $rStart = microtime(true);
-            $tempRoot = \App\node::find($cloneParentId);
-            $tempTree = $tempRoot->getDescendantsTree();
-            $readTime = microtime(true) - $rStart;
-            $this->command->info('read tree size: ' . ($tempRoot->countDescendants() + 1));
-            $this->command->info('read time: ' . $readTime);
+                $clonedNum = ($cloneNode->countDescendants() + 1);
+                $treeNum = ($cloneNodeParent->countDescendants() + 1);
+                $tableNum = \App\Node::count();
 
-            $cloneNodeParent = \App\node::find($cloneParentId);
+                $this->command->info('number of added nodes: ' . $clonedNum);
+                $this->command->info('nodes in tree: ' . $treeNum);
+                $this->command->info('nodes in table: ' . $tableNum);
+                \Illuminate\Support\Facades\DB::table('stats')->insert([
+                    'clone_time' => !empty($cloneTime) ? $cloneTime : null,
+                    'cloned_num' => !empty($clonedNum) ? $clonedNum : null,
+                    'tree_num' => $treeNum,
+                    'table_num' => $tableNum,
+                    'read_time' => $readTime
+                ]);
+                $this->command->info("-----------------------------------------");
+                $this->command->info('');
+                $this->command->info('');
+            }
 
-            $clonedNum = ($cloneNode->countDescendants() + 1);
-            $treeNum = ($cloneNodeParent->countDescendants() + 1);
-            $tableNum = \App\Node::count();
-
-            $this->command->info('number of added nodes: ' . $clonedNum);
-            $this->command->info('nodes in tree: ' . $treeNum);
-            $this->command->info('nodes in table: ' . $tableNum);
-            \Illuminate\Support\Facades\DB::table('stats')->insert([
-                'clone_time' => $cloneTime,
-                'cloned_num' => $clonedNum,
-                'tree_num' => $treeNum,
-                'table_num' => $tableNum,
-                'read_time' => $readTime
-            ]);
-            $this->command->info("-----------------------------------------");
-            $this->command->info('');
-            $this->command->info('');
+            $companyId++;
         }
 
         $end = microtime(true);
